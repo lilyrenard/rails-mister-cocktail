3ac9fcfed3470f92a91056ab6b05cb2e3a9db183
Lily Renard <renard.lily@gmail.com>
2018-10-19 10:04:12 +0200

    mister cocktail

---
+++ db/migrate/20181018091348_add_cocktail_to_ingredient.rb
@@ -0,0 +1,5 @@
+class AddCocktailToIngredient < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :ingredients, :cocktail, foreign_key: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "json"
+
+puts 'Creating ingredients...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+drinks_total = open(url).read
+
+drinks_h = JSON.parse(drinks_total)
+
+all_drinks = drinks_h["drinks"]
+all_drinks.each do |drink|
+  Ingredient.create(
+    name: drink["strIngredient1"]
+  )
+end
+
+puts 'Finished!'
